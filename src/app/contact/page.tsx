62acd3a0c791b72b926e9eb781537b1bc5b381cb
Kalhara Batangala <kalharabatangala@gmail.com>
2025-08-22 19:37:59 +0530

    fixed resend issues

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/getImageUrl', {
+      const response = await fetch('/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
